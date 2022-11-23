e35eab2794acdee9d52914f83e163fa9d072e66a
RyanDeMesa <ryandemesa09@gmail.com>
2022-11-23 21:58:55 +0000

    added to connect function: Name when entering game and message to say connection is success

--- client.js
+++ client.js
@@ -1,10 +1,18 @@
 // establishes a connection with the game server
+const { write } = require("fs");
 const net = require("net");
+
 const connect = function () {
  const conn = net.createConnection({
    host: "165.227.47.243",// IP address here,
    port: 50541,// PORT number here,
  });
+ conn.on("connect", () => {
+  console.log("Successfully connected to game server");
+ });
+ conn.on("connect", () => {
+  conn.write("Name: TBL")
+ })
  // interpret incoming data as text
  conn.setEncoding("utf8");
  conn.on("data", (data) => {
