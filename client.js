726503d06001ee990b5aa0863c4c652524bd01d2
RyanDeMesa <ryandemesa09@gmail.com>
2022-11-23 22:26:36 +0000

    experimented with moving to undertand the game server

--- client.js
+++ client.js
@@ -3,24 +3,30 @@ const { write } = require("fs");
 const net = require("net");
 
 const connect = function () {
- const conn = net.createConnection({
-   host: "165.227.47.243",// IP address here,
-   port: 50541,// PORT number here,
- });
- conn.on("connect", () => {
-  console.log("Successfully connected to game server");
- });
- conn.on("connect", () => {
-  conn.write("Name: TBL")
- })
- // interpret incoming data as text
- conn.setEncoding("utf8");
- conn.on("data", (data) => {
-  console.log(data);
- });
- return conn;
+  const conn = net.createConnection({
+    host: "165.227.47.243", // IP address here,
+    port: 50541, // PORT number here,
+  });
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+
+    conn.write("Name: RY");
+
+    conn.write("Move: up");
+
+    setInterval(() => {
+     conn.write("Move: up");
+    }, 50)
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+  return conn;
 };
 
 module.exports = {
- connect
+  connect,
 };
